366663f91f6e68ee6ea13e51c11995466bd84883
ZiBullet <zamolidinziadulaev@gmail.com>
2022-06-30 01:24:24 +0500

    Every thing is done!

---
+++ scripts/db.js
@@ -0,0 +1,18 @@
+export let users = [
+    {
+        name: 'Jimmy',
+        age: 12
+    },
+    {
+        name: 'Alisher',
+        age: 49
+    },
+    {
+        name: 'John',
+        age: 16
+    },
+    {
+        name: 'Jumshud',
+        age: 2006
+    },
+]
